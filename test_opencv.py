b607ac162dd18e309d5e28038174394566f12c78
Devin Hedge <devin@devinhedge.com>
2020-09-11 20:37:27 -0400

    Create test_opencv.py
    Initial commit. May be buggy. Certainly it doesn't seem to have any error handling. :-)

---
+++ test_opencv.py
@@ -0,0 +1,11 @@
+## Python script to quickly test the installation of OpenCV.
+##
+## Usage:
+## $ python3 test_opencv.py
+##
+## This should return the version number of the OpenCV installation and exit.
+##
+
+import cv2
+
+cv2.__version__
